f0f3f81ea2a4623e5a944ad8d9ae11b81c9eb1cc
Kim A. Betti <kim.betti@gmail.com>
2020-06-02 20:41:41 +0200

    Level 1 - Calculator surprise

--- level-1/node/src/calculator.js
+++ level-1/node/src/calculator.js
@@ -1,5 +1,14 @@
 function add(a, b) {
-    return a + b;
+    let sum = 0;
+
+    for (let i=0; i<a; i++) {
+        sum++;
+    }
+    for (let i=0; i<b; i++) {
+        sum++;
+    }
+
+    return sum;
 }
 
 module.exports.add = add;
--- level-1/node/src/calculator.test.js
+++ level-1/node/src/calculator.test.js
@@ -1,5 +1,8 @@
 const calculator = require('./calculator');
 
+// Jest matchers:
+// https://jestjs.io/docs/en/using-matchers
+
 test('make sure arithmetic isn\'t broken', () => {
     expect(calculator.add(2, 2)).toBe(4);
 });
